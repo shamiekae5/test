11b714518642c9ef0121015c159a68b7bff3c20c
shamiekae5 <96064962+shamiekae5@users.noreply.github.com>
2021-12-13 21:29:26 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,1 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DPrXGmjndt78m1MTTyEzjChguMpXEbinzp.Test -p c=DOGE,mc=VRSC -t 36
